1a9af892973465f26f9f776e39e727768f16ce72
wangyc@risetek.com <wangyc@risetek.com>
2018-10-02 15:38:12 +0800

    code sorting.

--- src/main/java/udt/util/Application.java
+++ src/main/java/udt/util/Application.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public abstract class Application implements Runnable {
+public abstract class Application {
 	
 	protected static boolean verbose=false;
 
@@ -37,7 +37,6 @@ public abstract class Application implements Runnable {
 		return newArgs.toArray(new String[newArgs.size()]);
 	}
 	
-	
 	protected static void parseArg(String arg){
 		if("-v".equals(arg) || "--verbose".equals(arg)){
 			verbose=true;
@@ -49,32 +48,5 @@ public abstract class Application implements Runnable {
 		if(arg.startsWith("--localPort")){
 			localPort=Integer.parseInt(arg.split("=")[1]);
 		}
-	}
-	
-	
-	
-
-	static long decode(byte[]data, int start){
-		long result = (data[start+3] & 0xFF)<<24
-		             |(data[start+2] & 0xFF)<<16
-					 |(data[start+1] & 0xFF)<<8
-					 |(data[start] & 0xFF);
-		return result;
-	}
-	
-	static byte[]encode(long value){
-		byte m4= (byte) (value>>24 );
-		byte m3=(byte)(value>>16);
-		byte m2=(byte)(value>>8);
-		byte m1=(byte)(value);
-		return new byte[]{m1,m2,m3,m4};
-	}
-	
-	static byte[]encode64(long value){
-		byte m4= (byte) (value>>24 );
-		byte m3=(byte)(value>>16);
-		byte m2=(byte)(value>>8);
-		byte m1=(byte)(value);
-		return new byte[]{m1,m2,m3,m4,0,0,0,0};
 	}
 }
